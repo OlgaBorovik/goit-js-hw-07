e4e9236f3c2f6808f73fc0d7166c566b4be3acb7
OlgaBorovik <jkz8686@gmail.com>
2022-08-14 21:47:48 +0300

    Update 01-gallery.js
    render markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+const galleryMarkUp = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `
+    ).join('')
+};
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp)
+
+
+console.log(createGalleryMarkup(galleryItems))
+
+
+// console.log(galleryContainer)
